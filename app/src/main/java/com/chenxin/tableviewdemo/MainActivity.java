f4c1ef9b17dedafd39b8c0139708b73fc367e3ab
chenxin <634620179@qq.com>
2017-07-20 11:38:02 +0800

    增加了一张演示图片

--- app/src/main/java/com/chenxin/tableviewdemo/MainActivity.java
+++ app/src/main/java/com/chenxin/tableviewdemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.chenxin.tableviewdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
